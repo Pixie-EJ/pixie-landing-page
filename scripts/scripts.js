6ab2c7f0ad6f67856f5ddce54910c42f6f4bbfb1
gabriel1larocca <gabriellarocca1@outlook.com>
2021-08-23 22:34:52 -0300

    remove js nao usado

--- scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-function responsiveNav() {
-  var x = document.getElementById("header");
-
-  if (x.className === "topbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "topbar";
-  }
-}
